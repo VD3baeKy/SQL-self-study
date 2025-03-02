8daa89ea9582da81e9839208f2637adefc35d4c5
VD3baeKy <digfehc@gmail.com>
2025-03-03 02:00:42 +0900

    Update and rename sample.sql to PostgreSQL_sample001.sql

---
+++ SQL/PostgreSQL/PostgreSQL_sample001.sql
@@ -0,0 +1,113 @@
+/* [PostgreSQL] このschema.sqlファイル内のSQL文はアプリが起動する度に実行されるため、
+   CREATE TABLE文にIF NOT EXISTSを追加して
+   「もしhousesという名前のテーブルが存在しなければ」という条件をつけている。 */
+CREATE TABLE IF NOT EXISTS houses (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    image_name VARCHAR(255),
+    description VARCHAR(255) NOT NULL,
+    price INT NOT NULL,
+    capacity INT NOT NULL,
+    postal_code VARCHAR(50) NOT NULL,
+    address VARCHAR(255) NOT NULL,
+    phone_number VARCHAR(50) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS roles (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL
+);
+ 
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    furigana VARCHAR(50) NOT NULL,
+    postal_code VARCHAR(50) NOT NULL,
+    address VARCHAR(255) NOT NULL,
+    phone_number VARCHAR(50) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    role_id INT NOT NULL,
+    enabled BOOLEAN NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (role_id) REFERENCES roles (id)
+);
+
+CREATE TABLE IF NOT EXISTS verification_tokens (
+    id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL UNIQUE,
+    token VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users (id)
+);
+
+CREATE TABLE IF NOT EXISTS reservations (
+    id SERIAL PRIMARY KEY,
+    house_id INT NOT NULL,
+    user_id INT NOT NULL,
+    checkin_date DATE NOT NULL,
+    checkout_date DATE NOT NULL,
+    number_of_people INT NOT NULL,
+    amount INT NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (house_id) REFERENCES houses (id),
+    FOREIGN KEY (user_id) REFERENCES users (id)
+);
+
+CREATE TABLE IF NOT EXISTS reviews (
+     id SERIAL PRIMARY KEY,
+     house_id INT NOT NULL,
+     user_id INT NOT NULL,
+     rating INT NOT NULL,
+     review_text VARCHAR(255),
+     image_name VARCHAR(255),
+     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+     updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS loves (
+     id SERIAL PRIMARY KEY,
+     house_id INT NOT NULL,
+     user_id INT NOT NULL,
+     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+/*
+CREATE OR REPLACE FUNCTION update_timestamp()
+RETURNS TRIGGER AS $$
+BEGIN
+  NEW.updated_at = NOW();
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER houses_table_modtime
+BEFORE UPDATE ON houses
+FOR EACH ROW
+EXECUTE PROCEDURE update_timestamp();
+
+CREATE TRIGGER users_table_modtime
+BEFORE UPDATE ON users
+FOR EACH ROW
+EXECUTE PROCEDURE update_timestamp();
+
+CREATE TRIGGER verification_tokens_table_modtime
+BEFORE UPDATE ON verification_tokens
+FOR EACH ROW
+EXECUTE PROCEDURE update_timestamp();
+
+CREATE TRIGGER reservations_table_modtime
+BEFORE UPDATE ON reservations
+FOR EACH ROW
+EXECUTE PROCEDURE update_timestamp();
+
+CREATE TRIGGER reviews_tokens_table_modtime
+BEFORE UPDATE ON reviews
+FOR EACH ROW
+EXECUTE PROCEDURE update_timestamp();
+*/
